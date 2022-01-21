1c00fba0d9aa381be07e156e993d89a294c0c82d
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-01-22 00:35:26 +0530

    added auth + postgresql

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,5 +1,7 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 const BlogLayout = ({ children }) => {
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
   return (
     <>
       <header>
@@ -14,6 +16,12 @@ const BlogLayout = ({ children }) => {
             <li>
               <Link to={routes.contact()}>Contact Me</Link>
             </li>
+            <li>
+              <button onClick={isAuthenticated ? logOut : logIn}>
+                {isAuthenticated ? 'Log Out' : 'Log In'}
+              </button>
+            </li>
+            {isAuthenticated && <li>{currentUser.email}</li>}
           </ul>
         </nav>
       </header>
